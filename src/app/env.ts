5c70433260a3ab8830ef4311bbc11facfe59716c
Adison Cavani <adriansrodon@tuta.io>
2022-12-15 22:05:16 +0100

    Split dotenv handling into separate files

--- src/app/env.ts
+++ src/app/env.ts
@@ -1,17 +1,9 @@
 import dotenv from "dotenv";
-import { z, TypeOf } from "zod";
+import { configSchema } from "./config";
 
 dotenv.config();
 
-const withDevDefault = <T extends z.ZodTypeAny>(schema: T, val: TypeOf<T>) =>
-  process.env["NODE_ENV"] !== "production" ? schema.default(val) : schema;
-
-const schema = z.object({
-  API_URL: withDevDefault(z.string().url(), "http://localhost"),
-  PORT: z.string().transform(Number),
-});
-
-const parsed = schema.safeParse(process.env);
+const parsed = configSchema.safeParse(process.env);
 
 if (!parsed.success) {
   console.error(
